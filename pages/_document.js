61bd91fb9b36fdc806b6c2f3182290972323f23c
soba00003 <email-dang-ky-github@domain.com>
2026-01-12 19:52:44 -0800

    Update articles

--- pages/_document.js
+++ pages/_document.js
@@ -5,25 +5,12 @@ export default function Document() {
   return (
     <Html lang="vi">
       <Head>
-        {/* Viewport + hiển thị */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="robots" content="max-image-preview:large" />
 
-        {/* Coinzilla verification (an toàn, có thể giữ) */}
-        <meta
-          name="coinzilla"
-          content="03663c7dfaaa9339ce67204520a48640"
-        />
+        <meta name="coinzilla" content="03663c7dfaaa9339ce67204520a48640" />
 
-        {/* Monumetric head code */}
-        <script
-          type="text/javascript"
-          src="//monu.delivery/site/e/4/7835ef-697c-4270-b513-1b2b6975d566.js"
-          data-cfasync="false"
-        ></script>
+        {/* Monumetric head code REMOVED */}
       </Head>
       <body>
         <Main />
